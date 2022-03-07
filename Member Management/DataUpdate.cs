128e1a1a13fc914e88b13c889b8527bb4518c14f
VictorRemotes <victor.chen@remotes.com.tw>
2022-03-07 17:45:42 +0800

    20220307

--- Member Management/DataUpdate.cs
+++ Member Management/DataUpdate.cs
@@ -6,19 +6,21 @@ using System.Threading;
 namespace Member_Management
 {
     [TestClass]
-    public class UnitTest1
+    public class DataUpdate
     {
         DateTime Today = DateTime.Now;
         private IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
         private string today;
-        private string DemoBackOfficeUrl = "http://admin-demo-ng.568win.com/login";
+        private string BackOfficeUrl = "http://admin-demo-ng.568win.com/login";
+        private string PlayerSiteUrl = "http://demo-ng-104.568win.com";
+
 
         [TestMethod]
         public void TestAgentUpdate()
         {
             today = Today.ToString("yyyyMMdd");
             //登入Company帳號
-            driver.Navigate().GoToUrl(DemoBackOfficeUrl);
+            driver.Navigate().GoToUrl(BackOfficeUrl);
             driver.Manage().Window.Maximize();
             driver.FindElement(By.Id("UsernameLogin")).Click();
             driver.FindElement(By.Id("UsernameLogin")).Clear();
@@ -33,15 +35,16 @@ namespace Member_Management
             driver.FindElement(By.XPath("//table[@id='table']/thead/tr/th/div/div[3]/div[2]/div[2]/button")).Click();
             driver.FindElement(By.XPath("//table[@id='table']/thead/tr/th/div/div[2]/button")).Click();
             Thread.Sleep(3000);
+
             driver.FindElement(By.XPath("//tbody[@id='account-list-render']/tr/td[5]/a/div")).Click();
             Thread.Sleep(3000);
             driver.FindElement(By.Id("FirstName")).Click();
             driver.FindElement(By.Id("FirstName")).Clear();
-            driver.FindElement(By.Id("FirstName")).SendKeys("QATesting" + today);
+            driver.FindElement(By.Id("FirstName")).SendKeys("AgentTest" + today);
 
             driver.FindElement(By.Id("LastName")).Click();
             driver.FindElement(By.Id("LastName")).Clear();
-            driver.FindElement(By.Id("LastName")).SendKeys("QATesting" + today);
+            driver.FindElement(By.Id("LastName")).SendKeys("AgentTest" + today);
 
             driver.FindElement(By.Id("Mobile")).Click();
             driver.FindElement(By.Id("Mobile")).Clear();
@@ -52,9 +55,9 @@ namespace Member_Management
             driver.FindElement(By.Id("btn_update_account_info")).Click();
 
             var firstname = driver.FindElement(By.Id("FirstName"));
-            Assert.AreEqual(firstname.GetAttribute("value"), "QATesting" + today);
+            Assert.AreEqual(firstname.GetAttribute("value"), "AgentTest" + today);
             var lastname = driver.FindElement(By.Id("LastName"));
-            Assert.AreEqual(lastname.GetAttribute("value"), "QATesting" + today);
+            Assert.AreEqual(lastname.GetAttribute("value"), "AgentTest" + today);
             Thread.Sleep(5000);
 
             //登出Conpany帳號
@@ -78,12 +81,15 @@ namespace Member_Management
             driver.FindElement(By.Id("PasswordLogin")).SendKeys("test" + today);
             driver.FindElement(By.Id("btn-login")).Click();
             Thread.Sleep(3000);
+            driver.Close();
 
         }
+
         [TestMethod]
         public void TestPlayUpdate()
         {
-            driver.Navigate().GoToUrl(DemoBackOfficeUrl);
+            today = Today.ToString("yyyyMMdd");
+            driver.Navigate().GoToUrl(BackOfficeUrl);
             driver.Manage().Window.Maximize();
             driver.FindElement(By.Id("UsernameLogin")).Click();
             driver.FindElement(By.Id("UsernameLogin")).Clear();
@@ -94,6 +100,45 @@ namespace Member_Management
             driver.FindElement(By.Id("btn-login")).Click();
             Thread.Sleep(3000);
 
+            driver.FindElement(By.XPath("//div[@id='maincontainer']/div[2]/div/span")).Click();
+            driver.FindElement(By.XPath("//div[@id='maincontainer']/div[2]/div[2]/ul/a[3]/li")).Click();
+            driver.FindElement(By.XPath("//table[@id='table']/thead/tr/th/div/div[3]/div[2]/div[2]/button")).Click();
+            driver.FindElement(By.XPath("//table[@id='table']/thead/tr/th/div/div[2]/button")).Click();
+            Thread.Sleep(3000);
+
+            driver.FindElement(By.XPath("//tbody[@id='account-list-render']/tr[3]/td[5]/a/div")).Click();
+            Thread.Sleep(3000);
+
+            driver.FindElement(By.Id("FirstName")).Click();
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.FindElement(By.Id("FirstName")).SendKeys("PlayerTest" + today);
+
+            driver.FindElement(By.Id("LastName")).Click();
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys("PlayerTest" + today);
+
+            driver.FindElement(By.Id("Mobile")).Click();
+            driver.FindElement(By.Id("Mobile")).Clear();
+            driver.FindElement(By.Id("Mobile")).SendKeys(today);
+
+            driver.FindElement(By.Id("Password")).Click();
+            driver.FindElement(By.Id("Password")).SendKeys("test" + today);
+            driver.FindElement(By.Id("btn_update_account_info")).Click();
+
+            var firstname = driver.FindElement(By.Id("FirstName"));
+            Assert.AreEqual(firstname.GetAttribute("value"), "PlayerTest" + today);
+            var lastname = driver.FindElement(By.Id("LastName"));
+            Assert.AreEqual(lastname.GetAttribute("value"), "PlayerTest" + today);
+            Thread.Sleep(5000);
+
+            driver.Navigate().GoToUrl(PlayerSiteUrl);
+            driver.FindElement(By.Name("username")).Click();
+            driver.FindElement(By.Name("username")).Clear();
+            driver.FindElement(By.Name("username")).SendKeys("qatesting02");
+            driver.FindElement(By.Id("Loginpassword")).Click();
+            driver.FindElement(By.Id("Loginpassword")).Clear();
+            driver.FindElement(By.Id("Loginpassword")).SendKeys("test20220307");
+            driver.FindElement(By.Id("btn-signin")).Click();
         }
     }
 }
---
+++ Member Management/SignUp.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+
+namespace Member_Management
+{
+    [TestClass]
+    public class SignUp
+    {
+        DateTime Today = DateTime.Now;
+        private IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+        private string today;
+        private string BackOfficeUrl = "http://admin-demo-ng.568win.com/login";
+        private string PlayerSiteUrl = "http://demo-ng-104.568win.com";
+
+        [TestMethod]
+        public void CompanyOffline()
+        {
+            today = Today.ToString("yyyyMMdd");
+            driver.Navigate().GoToUrl(PlayerSiteUrl);
+            driver.FindElement(By.LinkText("Join Now FREE")).Click();
+            Thread.Sleep(3000);
+
+            driver.FindElement(By.Id("RegisterUsername")).Click();
+            driver.FindElement(By.Id("RegisterUsername")).Clear();
+            driver.FindElement(By.Id("RegisterUsername")).SendKeys("OfflineTest" + today);
+
+            driver.FindElement(By.Id("Registerpassword")).Click();
+            driver.FindElement(By.Id("Registerpassword")).Clear();
+            driver.FindElement(By.Id("Registerpassword")).SendKeys("Password" + today);
+
+            driver.FindElement(By.Id("confirm_password")).Click();
+            driver.FindElement(By.Id("confirm_password")).Clear();
+            driver.FindElement(By.Id("confirm_password")).SendKeys("Password" + today);
+
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys(today + "@test.com");
+
+            driver.FindElement(By.Id("Phone")).Click();
+            driver.FindElement(By.Id("Phone")).Clear();
+            driver.FindElement(By.Id("Phone")).SendKeys(today);
+
+       
+            driver.FindElement(By.Id("sign-up-form")).Click();
+
+        }
+
+        [TestMethod]
+        public void AgentOffline()
+        {
+            today = Today.ToString("yyyyMMdd");
+
+        }
+
+    }
+}
